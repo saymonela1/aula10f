7f5e1672859e547cd4dc3c193da47db5c3785ffe
Saymon Johnson <109766858+saymonela1@users.noreply.github.com>
2022-09-12 11:46:07 -0300

    Add files via upload

---
+++ Aula10f/README.md
@@ -0,0 +1,2 @@
+# aula10f
+college java activity
---
+++ Aula10f/src/aula10f/Carros.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula10f;
+
+/**
+ *
+ * @author Saymon
+ */
+public class Carros {
+    int ano,modelo;
+    String placa, modelocarro;
+    
+    public void setAno(int ano) { this.ano=ano;}
+    public int getAno(){ return ano;}
+    
+    public void setModelo(int modelo) { this.modelo=modelo;}
+    public int getModleo (){ return modelo;}
+    
+    public void setPlaca(String placa) { this.placa=placa;}
+    public String getPlaca(){ return placa;}
+    
+    public void setModeloCarro (String modelocarro) {this .modelocarro=modelocarro;}
+    public String getModeloCarro(){return modelocarro;}
+    
+    public Carros (){
+    
+    }
+    
+    
+}
